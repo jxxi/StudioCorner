d1974e9c07be589a2ebb652bc57b14ba19d8b6e6
unknown <jasminewiggins90@yahoo.com>
2018-09-22 19:49:02 -0500

    Added styles for login signup

--- src/login.js
+++ src/login.js
@@ -51,6 +51,40 @@ class Login extends Component {
                 handleSignup={signupWasClickedCallback}
                 handleLogin={loginWasClickedCallback}
                 handleRecoverPassword={recoverPasswordWasClickedCallback}
+                usernameCustomLabel="Username"
+                passwordCustomLabel="Password"
+                passwordConfirmationCustomLabel="Confirm Password"
+                recoverPasswordCustomLabel="Forgot Password"
+                signupCustomLabel="Signup"
+                submitLoginCustomLabel="Login"
+                goToLoginCustomLabel="Login"
+                submitSignupCustomLabel="Signup"
+                goToSignupCustomLabel="Signup"
+                submitRecoverPasswordCustomLabel="Recover"
+                styles={{
+                      mainWrapper: { background: 'transparent', textAlign: 'center', opacity: '.98', fontFamily: 'Lato, sans-serif'},
+                      mainTitle: {  },
+                      flipper: { transition: '0.1s' },
+                      signup: {
+                        input: {  },
+                        recoverPassword: {},
+                        button: { border: '2px solid #7CD6CE', background: 'transparent' },
+                      },
+                      login: {
+                        wrapper: { background: 'transparent' },
+                        inputWrapper: {  },
+                        input: {  },
+                        recoverPasswordWrapper: {  },
+                        recoverPasswordButton: {  },
+                        button: { border: '2px solid #7CD6CE', background: 'transparent' },
+                      },
+                      recoverPassword: {
+                        wrapper: { background: 'transparent' },
+                        inputWrapper: {  },
+                        input: {  },
+                        button: { border: '2px solid #7CD6CE', background: 'transparent' },
+                      },
+                    }}
             />
         </div>
     );
